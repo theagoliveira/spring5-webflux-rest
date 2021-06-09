9891d4882fccbb7e280bdd1818eecde574713c44
Thiago Cavalcante <thiago.kun@gmail.com>
2021-06-09 02:44:19 -0300

    Add model and repo for Category

---
+++ src/main/java/guru/springframework/spring5webfluxrest/domain/Category.java
@@ -0,0 +1,16 @@
+package guru.springframework.spring5webfluxrest.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document
+public class Category {
+
+    @Id
+    private String id;
+    private String description;
+
+}
---
+++ src/main/java/guru/springframework/spring5webfluxrest/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.spring5webfluxrest.repositories;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import guru.springframework.spring5webfluxrest.domain.Category;
+
+public interface CategoryRepository extends ReactiveMongoRepository<Category, String> {}
